89cda20438a5f0850c3d03896ebbad1f230e2c78
Spark <47666579+saligia5353@users.noreply.github.com>
2021-10-19 22:07:06 -0400

    After deploying
    New version after deploying the Shiny App

--- README.md
+++ README.md
@@ -24,11 +24,22 @@ Term: Fall 2021
 	+ Ruopu Fan
 	+ Spark Li
 	+ Orianne Luo
+	
++ **Shiny app link**: [Reopen NYC Report](https://saligia5353.shinyapps.io/ReopenNYCReport/)
 
-+ **Project summary**: This project aims to illustrate how Covid-19 has affected the day to day lives of New York City Residents through interactive geographic plots of homeless populations, high school, and open businesses in New York City. We start by introducing the general effects of Covid-19 on the lives of the residents by looking at infection rates, hospitalization rates, and death rates over time. By comparing these statistics with the reported number of homeless families and individuals, as well as the number of open businesses over time, the goal of the project is to provide users with valuable insight on how Covid-19 has impacted all aspects of the life of New York City residents. 
++ **Source of the dataset**: 
+  + https://data.cityofnewyork.us/Health/COVID-19-Daily-Counts-of-Cases-Hospitalizations-an/rc75-m7u3
+  + https://data.cityofnewyork.us/Transportation/Open-Storefronts-Applications-Map-/bia8-jpx6
+  + https://data.cityofnewyork.us/Social-Services/Buildings-by-Borough-and-Community-District/3qem-6v3v
+  + https://data.cityofnewyork.us/Social-Services/Directory-Of-Homebase-Locations/ntcm-2w4k
+  + https://data.cityofnewyork.us/Social-Services/DHS-Daily-Report/k46n-sa2m
+
++ **Project summary**: This project aims to illustrate how COVID-19 has affected the day to day lives of New York City Residents through interactive geographic plots of homeless populations, high school, and open businesses in New York City. We start by introducing the general effects of COVID-19 on the lives of the residents by looking at infection rates, hospitalization rates, and death rates over time. By comparing these statistics with the reported number of homeless families and individuals, as well as the number of open businesses over time, the goal of the project is to provide users with valuable insight on how COVID-19 has impacted all aspects of the life of New York City residents. 
 The data was taken from NYC Open Data, found by sorting by agencies.
 
 + **Contribution statement**: ([default](doc/a_note_on_contributions.md)) All team members contributed equally in all stages of this project. All team members approve our work presented in this GitHub repository including this contributions statement. 
+
+To be more specific, we selected a part of Ruopu's visualizations and analysis as Cases by Borough section and Outdoor storefronts section; we selected a part of Orianne's visualizations and analysis as DHS Shelter System section. Spark is responsible for Case Count section and all tuning part. Corwin is responsible for all parts related to texts.
 
 Following [suggestions](http://nicercode.github.io/blog/2013-04-05-projects/) by [RICH FITZJOHN](http://nicercode.github.io/about/#Team) (@richfitz). This folder is orgarnized as follows.
 
--- app/ui.R
+++ app/ui.R
@@ -37,7 +37,7 @@ shinyUI(fluidPage(
     theme = shinytheme("sandstone"),
     
     # Application title
-    titlePanel("Reopen NYC"),
+    titlePanel("Reopen NYC Report"),
     
     navlistPanel(
         "Pandemic Data Now",
@@ -57,12 +57,17 @@ shinyUI(fluidPage(
                  br(),
                  br(),
                  br(),
-                 p("This section offers information about the  
-                    new, hospitalized and death cases caused by COVID-19 
-                 in the last 90 days in New York City"),
                  p(strong("Even though the death rate is significantly lower now, 
                  monitoring your and your family's physical condition is still 
-                           necessary!"))),
+                           necessary!")),
+                 br(),
+                 br(),
+                 br(),
+                 br(),
+                 br(),
+                 p("This section offers information about the  
+                    new, hospitalized and death cases caused by COVID-19 
+                 in the last 90 days in New York City")),
         
         tabPanel("Cases by Borough",
                  h4("Let's go further. How's the situation in each borough like
@@ -83,54 +88,63 @@ shinyUI(fluidPage(
                          dygraphOutput(outputId = "dygraph")
                      )
                  ),
-                p("This section offers information about the confirmed, probable,
-                  hospitalized and death cases caused by COVID-19 in 2021. You can
-                  use the range selector under each time series plot to have a closer
-                  look at any time period you want.")),
+                 br(),
+                 br(),
+                 br(),
+                 br(),
+                 br(),
+                 p("This section offers information about the confirmed, probable,
+                   hospitalized and death cases caused by COVID-19 in 2021. You 
+                   can use the range selector under each time series plot to 
+                   have a closer look at any time period you want.")),
         
         "Reopening Data Now",
         tabPanel("Outdoor storefronts", 
-                 h4("Check out all storefronts who want to use outdoor areas on a temporary
-           bases and take care!"),
+                 h4(strong("Check out all storefronts who want to use outdoor areas on 
+                 a temporary bases and take care!")),
                  leafletOutput("leafmap"),
-                p("Ground-floor storefront retailers in the New York City now 
-                  tend to use more outdoor areas because of the Covid-19 pandemic.
-                  Above map shows businesses applied for Open Storefronts, a program
-                  that assists in temporary outdoor area usage. By clicking on each circle,
-                  one can access information for a certain retailer available for outdoor seating,
-                  its address, business type and seating choice."),
-                p("Storefronts are adapting to Covid by using sidewalk seating etc. As shown by the interactive map, there
-                  are quite a few stores in New York City, reopening like so")),
+                 br(),
+                 p("Ground-floor storefront retailers in the New York City now 
+                   tend to use more outdoor areas because of the Covid-19 pandemic.
+                   Above map shows businesses applied for Open Storefronts, a program
+                   that assists in temporary outdoor area usage. By clicking on each circle,
+                   one can access information for a certain retailer available for outdoor seating,
+                   its address, business type and seating choice."),
+                 p("Storefronts are adapting to Covid by using sidewalk seating etc. As shown by the interactive map, there
+                   are quite a few stores in New York City, reopening like so.")),
         
         
         tabPanel("DHS Shelter System",
                  h4("Shelter Data"),
-                 p(strong("Question 1.1: How many people need assistance from the Department of Homeless Services?)")),
+                 p(strong("How many people need assistance from the Department 
+                          of Homeless Services since reopening?)")),
                  dygraphOutput("dygraph_1"),
+                 br(),
                  p("Looking at the data from 2020 onwards, we can see that there 
-                has been a general decrease in the number of people who have been 
-                reported to be homeless. There was a drop in the number people 
-                residing in the Department of Homeless Services in July of this year,
-                though the number has increased slightly since then."),
-                 p("This dip in reported homeless peope correates well with the low number of covid cases
-                   during that time of July. Meaning that there might be a link between the two. Drop in covid cases
-                   occuring at the same time at drop in reported homeless people"),
-                 p("Question 1.2: Where can individuals go for help, and what resources are available
-                  to them from the Department of Homeless Services? The two datasets used here were 
-                  https://data.cityofnewyork.us/Social-Services/Buildings-by-Borough-and-Community-District/3qem-6v3v
-                  and https://data.cityofnewyork.us/Social-Services/Directory-Of-Homebase-Locations/ntcm-2w4k
-                  ."),
+                   has been a general decrease in the number of people who have been 
+                   reported to be homeless. There was a drop in the number people 
+                   residing in the Department of Homeless Services in July of 
+                   this year, though the number has increased slightly since then."),
+                 p("This dip in reported homeless peope correates well with the 
+                   low number of covid cases during that time of July. Meaning 
+                   that there might be a link between the two. Drop in covid cases
+                   occuring at the same time at drop in reported homeless people."),
+                 br(),
+                 p(strong("Where can individuals go for help, and what resources
+                          are available to them from the Department of Homeless 
+                          Services?")),
                  leafletOutput("leafmap_1"),
-                 p("This map shows the location of homebase locations for the Department
-                  of Homeless Services, as well as the number of shelter buildings available
-                  at each of these community districts.")),
+                 p("This map shows the location of homebase locations for the 
+                   Department of Homeless Services (DHS), as well as the number 
+                   of shelter buildings available at each of these community 
+                   districts.")),
         "----------",
         tabPanel("About Project",
-                 h4("Reopen NYC"),
-                 p("Reopen NYC is a tool which provides daily COVID-19 update and
-                    useful information about NYC reopening. As project develops,
-                    more information about reopening such as storefront schedule,
-                    school policy will be added."),
+                 h4("Reopen NYC Report"),
+                 p("Reopen NYC Report is a tool which provides daily COVID-19 
+                   update and useful information about NYC reopening. 
+                   As project develops, more information about reopening such as
+                   storefront schedule, DHS Shelter System policy will be added."),
                  br(),
                  h4("Precaution"),
                  p("Sometime the gif or the leatlet map may not load successfully
@@ -143,36 +157,47 @@ shinyUI(fluidPage(
                  p("Ruopu Fan: rf2760@columbia.edu"),
                  p("Spark Li: hl3431@columbia.edu"),
                  p("Orianne Luo: ol2216@columbia.edu"),
-                h4("Data Sources"),
-                p("We sourced this data from  NYC Open Data, with the following links:"),
-                p("https://data.cityofnewyork.us/Health/COVID-19-Daily-Counts-of-Cases-Hospitalizations-an/rc75-m7u3"),
-                p("https://data.cityofnewyork.us/Transportation/Open-Storefronts-Applications-Map-/bia8-jpx6"),
-                p("https://data.cityofnewyork.us/Social-Services/Buildings-by-Borough-and-Community-District/3qem-6v3v"),
-                p("https://data.cityofnewyork.us/Social-Services/Directory-Of-Homebase-Locations/ntcm-2w4k."),
-                p("https://data.cityofnewyork.us/Social-Services/DHS-Daily-Report/k46n-sa2m"),
-                h4("Packages Used"),
-                p("We coded this app in Rstudio, using the following packages:
+                 br(),
+                 h4("Data Sources"),
+                 p("We sourced this data from  NYC Open Data, with the following links:"),
+                 p("https://data.cityofnewyork.us/Health/COVID-19-Daily-Counts-of-Cases-Hospitalizations-an/rc75-m7u3"),
+                 p("https://data.cityofnewyork.us/Transportation/Open-Storefronts-Applications-Map-/bia8-jpx6"),
+                 p("https://data.cityofnewyork.us/Social-Services/Buildings-by-Borough-and-Community-District/3qem-6v3v"),
+                 p("https://data.cityofnewyork.us/Social-Services/Directory-Of-Homebase-Locations/ntcm-2w4k."),
+                 p("https://data.cityofnewyork.us/Social-Services/DHS-Daily-Report/k46n-sa2m"),
+                 br(),
+                 h4("Packages Used"),
+                 p("We coded this app in Rstudio, using the following packages:
                   shiny, shinyWidgets, shinyThemes, leaflet, leafet.extras, dygraphs,
                   dpyr, magrittr, mapview, leafsync, gganimatem gifski, tidyr, and xts")),
+        
         tabPanel("Conclusions",
                  h4("Here is a write-up of the conclusions we observed from 
-                    our data analysis and analysis of the interactive graphs provided"),
-                 p("Through looking at the NYC data on Covid cases, represented by a gif in the first tab of this shiny app,
-                   we can see that while the death rate and hospitalization rates are now quite low, Covid is still
-                   spreading in New York City, with a fluctation between 500 and 2000 cases daily. This should be a reminder
-                   that while covid is less dangerous than it used to be, it is stil something to be concerned about in New York City"),
+                    our data analysis and analysis of the interactive graphs 
+                    provided."),
+                 p("Through looking at the NYC data on COVID-19 cases, represented 
+                   by a gif in the first tab of this shiny app, we can see that 
+                   while the death rate and hospitalization rates are now quite 
+                   low, COVID-19 is still spreading in New York City, with a 
+                   fluctation between 500 and 2000 cases daily. This should be 
+                   a reminder that while the pandemic is less dangerous than it 
+                   used to be, it is still something to be concerned about in New York City."),
                  br(),
                  p("Looking at the data from 2020 onwards, we can see that there 
-                has been a general decrease in the number of people who have been 
-                reported to be homeless. There was a drop in the number people 
-                residing in the Department of Homeless Services in July of this year,
-                though the number has increased slightly since then."),
-                 p("This dip in reported homeless peope correates well with the low number of covid cases
-                   during that time of July. Meaning that there might be a link between the two. Drop in covid cases
+                   has been a general decrease in the number of people who have 
+                   been reported to be homeless. There was a drop in the number 
+                   people residing in the Department of Homeless Services in July
+                   of this year, though the number has increased slightly since 
+                   then."),
+                 br(),
+                 p("This dip in reported homeless peope correates well with the 
+                   low number of covid cases during that time of July. Meaning 
+                   that there might be a link between the two. Drop in covid cases
                    occuring at the same time at drop in reported homeless people"),
                  br(),
-                 p("Storefronts are adapting to Covid by using sidewalk seating etc. As shown by the interactive map, there
-                  are quite a few stores in New York City, reopening like so"))
+                 p("Storefronts are adapting to Covid by using sidewalk seating 
+                   etc. As shown by the interactive map, there are quite a few 
+                   stores in New York City, reopening like so."))
     )
     
 ))
